c20ff348c9570022945c08a12f53c7386ced4f4b
newms34 <newms3450@gmail.com>
2016-06-23 15:28:07 -0400

    added login page to menu, added password 'salting', updated readme

--- README.md
+++ README.md
@@ -8,12 +8,12 @@ A lesson on magical AngularJS (1.x)!
  - [Author](#Author)
 
 ##Explanation:
-This is a relatively simple lesson on beginning AngularJS (version 1.x). There are three parts which are outlined below, but you should generally look at all of them to get a sense of how the parts fit together.
+This is a relatively simple lesson on beginning AngularJS (version 1.x). There are four parts which are outlined below, but you should generally look at all of them to get a sense of how the parts fit together.
 For those more advanced peeps, I've elected not to use either bower<sup>1</sup> or gulp/grunt<sup>2</sup> here, as while they make your http load times happier, I want to keep this demostration as Angular-focused as possible.
 The demonstration is focused, as I hint at above, on Angular 1, *not* Angular 2 (which is a whole different beast!).
 
 ##Parts:
-The demo consists of three files: the example, the template, and 'anatomy'.
+The demo consists of four files: the example, the template, 'anatomy', and the login demo page.
 
  - **Example**: This file (index.html) contains four example divs. The first is an old, non-JS div, and is generally speaking not interactive. The second is a vanilla JS div. It gets the job done, but with a lot of lines! The third div does the same thing as the second, but uses AngularJS (and thus fewer lines!). Finally, the fourth div is an example of angular in action
 
@@ -21,8 +21,10 @@ The demo consists of three files: the example, the template, and 'anatomy'.
 
  - **Anatomy**: This file (anato.html) shows the code and explanation for some of the essential AngularJS components, such as modules, controllers, factories, and directives. Take a look at each of the components and see how they interact with each other.
 
+ - **Login**: This file (login.html) is a very rough example of how Angular might handle user registration/login. Particularly, it focuses on front-end validation (i.e., did the user enter the same password twice correctly?). It includes an example of password 'salting'.
+
  ##Author:
- This demonstration was designed and written by me, [David Newman](https://github.com/newms34) 
+ This demonstration was designed and written by me, [David Newman](https://github.com/newms34). Feel free to visit my [LinkedIn](https://www.linkedin.com/in/newms34) too, if you want.
 
 <sup>1</sup><small> Bower allows you to load extraneous JS files on the front end. It's particularly nice because it includes some very popular libraries (like Angular and jQuery). Furthermore, it allows you to have more of a single-point-of-failure: if your browser can't load the AngularJS file, it likely won't be able to load anything else either!</small><br/>
 <sup>2</sup><small>Gulp and Grunt allow you to take a bunch of files and 'compress' them into one file. They also allow you to do cool things like compiling SASS (variables in CSS!) or minify your code (less bandwidth)!</small>
--- js/login/lmain.js
+++ js/login/lmain.js
@@ -1,10 +1,11 @@
 var app = angular.module('logApp', []);
 app.controller('logCont', function($scope) {
-    $scope.User = function(usr, pwd, time) {
+    $scope.User = function(usr, pwd, time, salt) {
         this.user = usr;
         this.pwd = pwd;
         this.loginNums = 0;
         this.lastLogin = time;
+        this.salt = salt;
     }
     $scope.logins = [];
     $scope.register = function() {
@@ -17,29 +18,64 @@ app.controller('logCont', function($scope) {
             return false;
         }
         alert('Thanks, ' + $scope.regname + '!');
-        $scope.logins.push(new $scope.User($scope.regname, $scope.regpwd, new Date()));
+        var encPwd = $scope.genPwd($scope.regpwd);
+        $scope.logins.push(new $scope.User($scope.regname, encPwd.pwd, new Date(), encPwd.salt));
         console.log($scope.logins)
     }
     $scope.login = function() {
-        var bad = true;
-        var found = -1;
-        for (var i = 0; i < $scope.logins.length; i++) {
-            //loop thru every user in the $scope.logins array. if we find 
-            if (($scope.logins[i].user == $scope.logname) && ($scope.logins[i].pwd == $scope.logpwd)) {
-                //found em!
-                bad = false;
-                found = i;
+            var bad = true;
+            var found = -1;
+            for (var i = 0; i < $scope.logins.length; i++) {
+                //loop thru every user in the $scope.logins array. if we find 
+                if (($scope.logins[i].user == $scope.logname)) {
+                    if ($scope.checkPwd($scope.logins[i], $scope.logpwd)) {
+                        //found em!
+                        bad = false;
+                        found = i;
+                    }
+                }
             }
+            if (bad) {
+                alert('Username/password not found! Please try again.');
+                return false;
+            }
+            $scope.logins[found].loginNums++;
+            var oldLogin = $scope.logins[found].lastLogin;
+            $scope.logins[found].lastLogin = new Date();
+            var elapsed = parseInt(($scope.logins[found].lastLogin.getTime() - oldLogin.getTime()) / 1000);
+            console.log(oldLogin.getTime())
+            alert('Hi, ' + $scope.logins[found].user + '. Welcome back! You\'ve logged in ' + $scope.logins[found].loginNums + ' times, and the last time you logged in was ' + oldLogin + '. This was ' + elapsed + ' seconds ago!');
         }
-        if (bad) {
-            alert('Username/password not found! Please try again.');
+        // The following is a (very small!) intro to Auth stuff
+    $scope.genPwd = function() {
+        //important note: Unless you're a security/encryption expert, writing your own encryption is generally a VERY BAD IDEA. This is absolutely something you wanna let the professionals handle. However, for the sake of this demo, I'll write mine.
+        var raw = $scope.regpwd,
+            //the 'salt' is a special type of encryption. It's saved on the back-end and basically serves as the decryption key for the actual password (which itself is stored in encrypted form)
+            salt = Math.ceil(Math.random() * parseInt(new Array(raw.length).fill(9, 0, raw.length).join('')));
+        if (!raw || raw == '') {
+            //if user didn't enter a password, throw an error and quit.
+            throw new Error('No password!');
             return false;
         }
-        $scope.logins[found].loginNums++;
-        var oldLogin = $scope.logins[found].lastLogin;            
-        $scope.logins[found].lastLogin = new Date();
-        var elapsed =  parseInt(($scope.logins[found].lastLogin.getTime() - oldLogin.getTime())/1000);
-        console.log(oldLogin.getTime())
-        alert('Hi, ' + $scope.logins[found].user + '. Welcome back! You\'ve logged in ' + $scope.logins[found].loginNums + ' times, and the last time you logged in was ' + oldLogin + '. This was '+elapsed+' seconds ago!');
+        var rawArr = raw.split('');
+        for (var i = 0; i < rawArr.length; i++) {
+            var num = rawArr[i].charCodeAt(0);
+            num += parseInt(salt.toString()[i]); //add the random num from salt to 'shift' the char. 
+            rawArr[i] = String.fromCharCode(num);
+        }
+        var encodedPwd = {
+            pwd: rawArr.join(''),
+            salt: salt.toString()
+        }
+        //So basically, for each character in the original password, we've shifted its char code by a random number. We then convert that char code BACK to a string, and store that (along with the salt so we can convert back!)
+        return encodedPwd;
+    }
+    $scope.checkPwd = function(usr, candidate) {
+        var decodedPwd = '';
+        for (var i = 0; i < usr.pwd.length; i++) {
+            var theLetter = usr.pwd[i].charCodeAt(0);
+            decodedPwd += String.fromCharCode(theLetter - parseInt(usr.salt[i]));
+        }
+        return true;
     }
 });
